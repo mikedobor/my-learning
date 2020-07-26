99f323edb4b046690cf7c7b7900429653a6d77fa
Michael Aigboeghian <micki1.com@gmail.com>
2020-07-26 18:37:22 +0100

    My React learning project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+function App() {
+  return (
+    <div className="App">
+      <Person />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const person = (props) => {
+    return [
+        <h1>Working on my React Skills</h1>,
+        <p>My daily goal is to study and code 2 hours everyday of the week</p>,
+        <p>I am using multiple elements in the return statement. I achieved this using square brackets(array) with each element seperated by a comma. React is cool!</p>
+    ]
+}
+
+export default person;
